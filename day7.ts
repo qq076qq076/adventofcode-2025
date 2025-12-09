8b0f9f23e7e79e011ddceb6581ece8f37b3ab562
David acer wsl <qq076qq076@gmail.com>
2025-12-10 02:39:45 +0800

    day7 part 2

--- day7.ts
+++ day7.ts
@@ -21,9 +21,30 @@ function lightArea(row: string[], tachyonList: Set<number>) {
   return newTachyonList;
 }
 
-let tachyonList = new Set<number>([startIndex]);
-for (let index = 1; index < inputList.length; index++) {
-  tachyonList = lightArea(inputList[index], tachyonList);
+function getTimes(row: string[], times: number[]) {
+  const newTimes: number[] = Array(row.length).fill(0);
+  times.forEach((time, index) => {
+    if (row[index] === "^") {
+      newTimes[index - 1] = (newTimes[index - 1] || 0) + time;
+      newTimes[index + 1] = (newTimes[index + 1] || 0) + time;
+    } else if (row[index] === ".") {
+      newTimes[index] = newTimes[index] + time;
+    }
+  });
+  return newTimes;
 }
 
-console.log(sum);
+let tachyonList = new Set<number>([startIndex]);
+let times: number[] = Array(inputList[0].length).fill(0);
+times[startIndex] = 1;
+for (let index = 1; index < inputList.length; index++) {
+  if (inputList[index].findIndex((text) => text == "^") === -1) {
+    continue;
+  }
+//   tachyonList = lightArea(inputList[index], tachyonList);
+  times = getTimes(inputList[index], times);
+}
+
+// console.log(sum);
+
+console.log(times.reduce((acc, curr) => acc + curr, 0));
